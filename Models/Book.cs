e6803fabb73ae46ddce38551fb49ba9fac8d2a8b
petrykovvladyslav <petrykovvladyslav@gmail.com>
2021-09-27 14:48:27 +0300

    test

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,6 +6,7 @@ namespace backend_dockerAPI.Models
     public class Book
     {
         [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
         [BsonElement("Name")]
