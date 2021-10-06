2e9b1276c01aac27358459d59f0663a0f3cd12c2
petrykovvladyslav <petrykovvladyslav@gmail.com>
2021-10-06 15:04:09 +0300

    more fields for quiz object

--- Models/Quiz.cs
+++ Models/Quiz.cs
@@ -9,6 +9,9 @@ namespace backend_dockerAPI.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string CreatorId { get; set; }
+        public string Name { get; set; }
+        public string RequiredStack { get; set; }
+        public int duration { get; set; }
         public string[] QuestionIds { get; set; }
         public int Complexity { get; set; }
     }
