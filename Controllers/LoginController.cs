20847bcce8649107b42ab938654ec1d54861889c
petrykovvladyslav <petrykovvladyslav@gmail.com>
2021-10-25 00:25:36 +0300

    fixed pipeline

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace backend_dockerAPI.Controllers
 {
-    [ApiRoutes.Login]
+    [Route("api/[controller]")]
     [ApiController]
     public class LoginController : Controller
     {
