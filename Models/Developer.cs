08f5a23f96c2c72f86574867bbef744c3f414d53
petrykovvladyslav <petrykovvladyslav@gmail.com>
2021-10-27 14:55:07 +0300

    changes for developer model

--- Models/Developer.cs
+++ Models/Developer.cs
@@ -8,13 +8,15 @@ namespace backend_dockerAPI.Models
         public string PhoneNumber { get; set; }
         public string City { get; set; }
         public string Description { get; set; }
+        public string OccupationFiled { get; set; }
 
-        public Developer(string email, string password, string name, string img, string[] solvedQuizzesIds, string phoneNumber, string city, string description) : base(email, password, name, img)
+        public Developer(string email, string password, string name, string img, string[] solvedQuizzesIds, string phoneNumber, string city, string description, string occupationFiled) : base(email, password, name, img)
         {
             SolvedQuizzesIds = solvedQuizzesIds;
             PhoneNumber = phoneNumber;
             City = city;
             Description = description;
+            OccupationFiled = occupationFiled;
         }
 
     }
