b3db831fd8aff63d129c151f54212bb00025042a
petrykovvladyslav <petrykovvladyslav@gmail.com>
2021-10-29 15:54:55 +0300

    reverted worked image controller

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -5,40 +5,52 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace backend_dockerAPI.Controllers 
+namespace backend_dockerAPI.Controllers
 {
- [Route("api/[controller]")]
- public class ImageController: ControllerBase {
-  public static IWebHostEnvironment _environment;
-  public ImageController(IWebHostEnvironment environment) {
-   _environment = environment;
-  }
-  public class FIleUploadAPI {
-   public IFormFile files {
-    get;
-    set;
-   }
-  }
-  [HttpPost]
-  public async Task < string > Post(FIleUploadAPI objfile) {
-      Console.WriteLine(_environment.WebRootPath);
-   if (objfile.files.Length > 0) {
-    try {
-     if (!Directory.Exists(Path.Combine(_environment.WebRootPath, "uploads"))) {
-      Directory.CreateDirectory(Path.Combine(_environment.WebRootPath, "uploads"));
-     }
-     using(FileStream filestream = System.IO.File.Create(Path.Combine(_environment.WebRootPath, "uploads", objfile.files.FileName))) {
-      objfile.files.CopyTo(filestream);
-      filestream.Flush();
-      return "/uploads/" + objfile.files.FileName;
-     }
-    } catch (Exception ex) {
-     return ex.ToString();
+    [Route("api/[controller]")]
+    public class ImageController : ControllerBase
+    {
+        public static IWebHostEnvironment _environment;
+        public ImageController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public class FIleUploadAPI
+        {
+            public IFormFile files
+            {
+                get;
+                set;
+            }
+        }
+        
+        [HttpPost]
+        public async Task<string> Post(FIleUploadAPI objfile)
+        {
+            if (objfile.files.Length > 0)
+            {
+                try
+                {
+                    if (!Directory.Exists(Path.Combine(_environment.WebRootPath, "uploads")))
+                    {
+                        Directory.CreateDirectory(Path.Combine(_environment.WebRootPath, "uploads"));
+                    }
+                    await using (FileStream filestream = System.IO.File.Create(Path.Combine(_environment.WebRootPath, "uploads", objfile.files.FileName)))
+                    {
+                        objfile.files.CopyTo(filestream);
+                        filestream.Flush();
+                        return "/uploads/" + objfile.files.FileName;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return ex.ToString();
+                }
+            }
+            else
+            {
+                return "Unsuccessful";
+            }
+        }
     }
-   } else {
-    return "Unsuccessful";
-   }
-
-  }
- }
-} 
+}
